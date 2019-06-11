6e3840808405c5e512ec0c7a3a9ea3c44ed45780
jtimbrook93 <jtimbroo@iu.edu>
2019-06-10 21:24:09 -0400

    connection test

--- app/environment.default.php
+++ app/environment.default.php
@@ -1,10 +1,6 @@
 <?php
 
-const DB_USER = 'user';
-const DB_PW = 'pw';
-const DB_SERVER = 'mysql:host=localhost;dbname=finalProject';
-
 $client = new MongoDB\Client(
-    'mongodb+srv://Jtimbrook93:<password>@iot-sjtus.mongodb.net/test?retryWrites=true&w=majority');
+    'mongodb+srv://Jtimbrook93:Biggi1yo@iot-sjtus.mongodb.net/test?retryWrites=true&w=majority');
 
 $db = $client->test;
