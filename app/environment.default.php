0f2f69f410bd909981018bf14bc1bed768fef4aa
jtimbrook93 <jtimbroo@iu.edu>
2019-06-10 21:31:39 -0400

    connection

--- app/environment.default.php
+++ app/environment.default.php
@@ -1,6 +1,9 @@
 <?php
+// connect to server
 
-$client = new MongoDB\Client(
+$client = new MongoDBClient(
     'mongodb+srv://Jtimbrook93:Biggi1yo@iot-sjtus.mongodb.net/test?retryWrites=true&w=majority');
+
+//connect to database
 
 $db = $client->test;
