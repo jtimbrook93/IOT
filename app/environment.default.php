59a5ca2c9c9dac6d530e547418e381b032c9a259
jtimbrook93 <jtimbroo@iu.edu>
2019-06-11 08:09:59 -0400

    encoding special char

--- app/environment.default.php
+++ app/environment.default.php
@@ -2,7 +2,7 @@
 // connect to server
 
 $client = new MongoDBClient(
-    'mongodb+srv://Jtimbrook93:Biggi1yo@iot-sjtus.mongodb.net/test?retryWrites=true&w=majority');
+    'mongodb+srv://Jtimbrook93:Biggi1%21yo@iot-sjtus.mongodb.net/test?retryWrites=true&w=majority');
 
 //connect to database
 
