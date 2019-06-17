e44c8d2fa1f48ae992e07cc8356f53853e4efd5c
jtimbrook93 <jtimbroo@iu.edu>
2019-06-17 18:04:11 -0400

    update

--- public/test.php
+++ public/test.php
@@ -1,6 +1,6 @@
 <?php
 
-   require '/home/ec2-user/vendor/autoload.php';
+   require '/vendor/autoload.php';
    // connect to mongodb
    $m = new MongoDB\Driver\Manager("mongodb://localhost:27017");
    echo "Connection to database successfully";
