3b6714ad91d5b28039c3e32159f886941b5663fb
jtimbrook93 <jtimbroo@iu.edu>
2019-06-17 19:57:24 -0400

    asdf

--- public/test.php
+++ public/test.php
@@ -2,8 +2,8 @@
 
    require '/var/www/html/vendor/autoload.php';
    // connect to mongodb
- $client = new MongoDB\Client(‘mongodb+srv://Jtimbrook93:Biggi1%21yo@iot-sjtus.mongodb.net/admin?retryWrites=true&w=majority’);
- ;
+ $client = new MongoDB\Client("mongodb+srv://Jtimbrook93:Biggi1%21yo@iot-sjtus.mongodb.net/admin?retryWrites=true&w=majority");
+
 
    echo "Connection to database successfully";
 
