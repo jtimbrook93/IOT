3179992b46c0f20ae6502066642f6f13125e1b5f
jtimbrook93 <jtimbroo@iu.edu>
2019-06-17 17:25:25 -0400

    asdf

--- public/test.php
+++ public/test.php
@@ -1,6 +1,6 @@
 <?php
    // connect to mongodb
-   $m = new MongoClient();
+   $m = new MongoDB\Driver\Manager("mongodb://localhost:27017");
    echo "Connection to database successfully";
 
    // select a database
