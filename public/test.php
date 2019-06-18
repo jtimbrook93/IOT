4a93a8988dbef0b4f20779b5432f198773fe7e5e
jtimbrook93 <jtimbroo@iu.edu>
2019-06-18 19:51:07 -0400

    asdf

--- public/test.php
+++ public/test.php
@@ -7,8 +7,29 @@ $PATH = "/usr/bin";
 
  require '/var/www/html/vendor/autoload.php';
    // connect to mongodb
- $client = new MongoDB\Driver\Manager("mongodb+srv://Jtimbrook93:Biggi1%21yo@iot-sjtus.mongodb.net/JT?retryWrites=true&w=majority");
+ //$client = new MongoDB\Driver\Manager("mongodb+srv://Jtimbrook93:Biggi1%21yo@iot-sjtus.mongodb.net/JT?retryWrites=true&w=majority");
 
+ $user = "Jtimbrook93";
+ $pwd = 'Biggi1%21yo';
+ $filter = [];
+
+ if (isset($_POST['needleID'])) {
+     $needleID = $_POST['needleID'];
+     $filter = ['id'=> $needleID];
+ }
+
+ //Manager Class
+ $connection = new MongoDB\Driver\Manager("mongodb://${user}:${pwd}@iot-sjtus.mongodb.net/JT?retryWrites=true&w=majority");
+
+ // Query Class
+ $query = new MongoDB\Driver\Query($filter);
+
+ // Output of the executeQuery will be object of MongoDB\Driver\Cursor class
+ $rows = $connection->executeQuery('sedwe.defaultConfig', $query);
+
+ // Convert rows to Array and sedn result back to client
+ $rowsArr = $rows->toArray();
+ echo json_encode($rowsArr);
 
    echo "Connection to database successfully";
 
