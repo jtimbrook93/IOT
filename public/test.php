85a32699ccf1c3dee3397984264f38403cd04a87
jtimbrook93 <jtimbroo@iu.edu>
2019-06-18 00:15:07 -0400

    asdf

--- public/test.php
+++ public/test.php
@@ -1,8 +1,8 @@
 <?php
 
-$PATH = "C:\php_mongodb\mongodb-linux-x86_64-amazon-4.0.10/bin"
+$PATH = "C:\php_mongodb\mongodb-linux-x86_64-amazon-4.0.10/bin";
 
-   public function require '/var/www/html/vendor/autoload.php';
+ require '/var/www/html/vendor/autoload.php';
    // connect to mongodb
  $client = new MongoDB\Driver\Manager("mongodb+srv://Jtimbrook93:Biggi1%21yo@iot-sjtus.mongodb.net/JT?retryWrites=true&w=majority");
 
