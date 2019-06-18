5e2d1d5ad07b4f25776d5ff666ced139a997b450
jtimbrook93 <jtimbroo@iu.edu>
2019-06-17 20:39:27 -0400

    asdf

--- public/test.php
+++ public/test.php
@@ -1,9 +1,11 @@
 <?php
+chdir(__DIR__);
+set_include_path (__DIR__);
 
    require '/var/www/html/vendor/autoload.php';
    // connect to mongodb
  $client = new MongoDB\Driver\Manager("mongodb+srv://Jtimbrook93:Biggi1%21yo@iot-sjtus.mongodb.net/admin?retryWrites=true&w=majority");
-var_dump($client);
+
 
    echo "Connection to database successfully";
 
