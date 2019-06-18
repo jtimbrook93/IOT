c2f820b952f9bcfc38dcb47ce353e21991307a1f
jtimbrook93 <jtimbroo@iu.edu>
2019-06-17 20:51:01 -0400

    asdf

--- public/test.php
+++ public/test.php
@@ -23,7 +23,7 @@ set_include_path (__DIR__);
       "url" => "http://www.tutorialspoint.com/mongodb/",
       "by" => "tutorials point"
    );
-   vardump($db $collection $client)
+   vardump($db, $collection, $clien)
 
    $collection->insert($document);
    echo "Document inserted successfully";
