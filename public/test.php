20cd5e43e41ab394f9e5a82a44b7f9bd4f4ac118
jtimbrook93 <jtimbroo@iu.edu>
2019-06-19 08:48:42 -0400

    asdf

--- public/test.php
+++ public/test.php
@@ -1,41 +1,43 @@
 <?php
 
-chdir(__DIR__);
-set_include_path (__DIR__);
-
-$PATH = "/usr/bin";
-
- require '/var/www/html/vendor/autoload.php';
-
-
- $user = "Jtimbrook93";
- $pwd = 'Biggi1%21yo';
- $filter = [];
-
- if (isset($_POST['needleID'])) {
-     $needleID = $_POST['needleID'];
-     $filter = ['id'=> $needleID];
- }
-
- //Manager Class
- $manager = new MongoDB\Driver\Manager("mongodb://${user}:${pwd}@iot-iot-shard-00-00-sjtus.mongodb.net:27017,iot-shard-00-01-sjtus.mongodb.net:27017,iot-shard-00-02-sjtus.mongodb.net:27017/test?ssl=true&replicaSet=IOT-shard-0&authSource=admin&retryWrites=true&w=majority");
-
-
-   echo "Connection to database successfully";
-
-   // select a database
-   $db = $manager->test;
-   echo "Database admin selected";
-   $collection = $db->test;
-   echo "Collection selected succsessfully";
-
-   $document = array(
-      "title" => "MongoDB",
-      "description" => "database",
-      "likes" => 100,
-      "url" => "http://www.tutorialspoint.com/mongodb/",
-      "by" => "tutorials point"
-   );
-
-   $collection->insert($document);
-   echo "Document inserted successfully";
+echo phpinfo();
+//
+// chdir(__DIR__);
+// set_include_path (__DIR__);
+//
+// $PATH = "/usr/bin";
+//
+//  require '/var/www/html/vendor/autoload.php';
+//
+//
+//  $user = "Jtimbrook93";
+//  $pwd = 'Biggi1%21yo';
+//  $filter = [];
+//
+//  if (isset($_POST['needleID'])) {
+//      $needleID = $_POST['needleID'];
+//      $filter = ['id'=> $needleID];
+//  }
+//
+//  //Manager Class
+//  $manager = new MongoDB\Driver\Manager("mongodb://${user}:${pwd}@iot-iot-shard-00-00-sjtus.mongodb.net:27017,iot-shard-00-01-sjtus.mongodb.net:27017,iot-shard-00-02-sjtus.mongodb.net:27017/test?ssl=true&replicaSet=IOT-shard-0&authSource=admin&retryWrites=true&w=majority");
+//
+//
+//    echo "Connection to database successfully";
+//
+//    // select a database
+//    $db = $manager->test;
+//    echo "Database admin selected";
+//    $collection = $db->test;
+//    echo "Collection selected succsessfully";
+//
+//    $document = array(
+//       "title" => "MongoDB",
+//       "description" => "database",
+//       "likes" => 100,
+//       "url" => "http://www.tutorialspoint.com/mongodb/",
+//       "by" => "tutorials point"
+//    );
+//
+//    $collection->insert($document);
+//    echo "Document inserted successfully";
