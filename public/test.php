8994d3e5a9ba678c2e3a700cbc54dcc07f85ecba
jtimbrook93 <jtimbroo@iu.edu>
2019-06-19 08:18:15 -0400

    asdf

--- public/test.php
+++ public/test.php
@@ -6,8 +6,7 @@ set_include_path (__DIR__);
 $PATH = "/usr/bin";
 
  require '/var/www/html/vendor/autoload.php';
-   // connect to mongodb
- //$client = new MongoDB\Driver\Manager("mongodb+srv://Jtimbrook93:Biggi1%21yo@iot-sjtus.mongodb.net/JT?retryWrites=true&w=majority");
+
 
  $user = "Jtimbrook93";
  $pwd = 'Biggi1%21yo';
@@ -21,15 +20,6 @@ $PATH = "/usr/bin";
  //Manager Class
  $manager = new MongoDB\Driver\Manager("mongodb://${user}:${pwd}@iot-iot-shard-00-00-sjtus.mongodb.net:27017,iot-shard-00-01-sjtus.mongodb.net:27017,iot-shard-00-02-sjtus.mongodb.net:27017/test?ssl=true&replicaSet=IOT-shard-0&authSource=admin&retryWrites=true&w=majority");
 
- // // Query Class
- // $query = new MongoDB\Driver\Query($manager);
- //
- // // Output of the executeQuery will be object of MongoDB\Driver\Cursor class
- // $rows = $manager->executeQuery('sedwe.defaultConfig', $query);
- //
- // // Convert rows to Array and sedn result back to client
- // $rowsArr = $rows->toArray();
- // echo json_encode($rowsArr);
 
    echo "Connection to database successfully";
 
