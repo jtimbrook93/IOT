3add73f1b2ed292d6e29dbf47bb765857fd21f35
jtimbrook93 <jtimbroo@iu.edu>
2019-06-24 17:39:06 -0400

    asdf

--- public/test.php
+++ public/test.php
@@ -1,6 +1,6 @@
 <?php
 
-echo phpinfo();
+//echo phpinfo();
 
 
 // // chdir(__DIR__);
@@ -14,27 +14,34 @@ require_once '/var/www/html/vendor/autoload.php';
  $user = "Jtimbrook93";
  $pwd = 'Biggi1%21yo';
 
+ try {
+    $connection = new MongoDB\Driver\Manager('mongodb+srv://${user}:${pwd}@iot-sjtus.mongodb.net/JT?retryWrites=true&w=majority');
+    $db = $connection->JT;
+    $collection = $db->IOT;
+    var_dump($collection);
+ } catch(\Exception $e) {
+   echo $e->getMessage(), "\n";
+ }
 
-
- //Manager Class
- $connection = new MongoDB\Driver\Manager('mongodb+srv://${user}:${pwd}@iot-sjtus.mongodb.net/test?retryWrites=true&w=majority');
-
-
-   echo "Connection to database successfully";
-
-   // select a database
-   $db = $connection->test;
-   echo "Database admin selected";
-   $collection = $db->test;
-   echo "Collection selected succsessfully";
-
-   $document = array(
-      "title" => "MongoDB",
-      "description" => "database",
-      "likes" => 100,
-      "url" => "http://www.tutorialspoint.com/mongodb/",
-      "by" => "tutorials point"
-   );
-
-   $collection->insert($document);
-   echo "Document inserted successfully";
+ // //Manager Class
+ // $connection = new MongoDB\Driver\Manager('mongodb+srv://${user}:${pwd}@iot-sjtus.mongodb.net/test?retryWrites=true&w=majority');
+ //
+ //
+ //   echo "Connection to database successfully";
+ //
+ //   // select a database
+ //   $db = $connection->test;
+ //   echo "Database admin selected";
+ //   $collection = $db->test;
+ //   echo "Collection selected succsessfully";
+ //
+ //   $document = array(
+ //      "title" => "MongoDB",
+ //      "description" => "database",
+ //      "likes" => 100,
+ //      "url" => "http://www.tutorialspoint.com/mongodb/",
+ //      "by" => "tutorials point"
+ //   );
+ //
+ //   $collection->insert($document);
+ //   echo "Document inserted successfully";
