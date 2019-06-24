40e81844bea4d90a77a39b6ff787d0f9cc28b3de
jtimbrook93 <jtimbroo@iu.edu>
2019-06-24 18:30:13 -0400

    asdf

--- public/test.php
+++ public/test.php
@@ -15,7 +15,7 @@ require_once '/var/www/html/vendor/autoload.php';
  $pwd = 'Biggi1%21yo';
 
  try {
-    $connection = new MongoDB\Driver\Manager('mongodb+srv://${user}:${pwd}@iot-shard-00-00-sjtus.mongodb.net:27017,iot-shard-00-01-sjtus.mongodb.net:27017,iot-shard-00-02-sjtus.mongodb.net:27017/test?ssl=true&replicaSet=IOT-shard-0&authSource=admin&retryWrites=true&w=majority');
+    $connection = new MongoDB\Driver\Manager('mongodb+srv://${user}:${pwd}@iot-shard-00-00-sjtus.mongodb.net:27017,iot-shard-00-01-sjtus.mongodb.net:27017,iot-shard-00-02-sjtus.mongodb.net:27017/JT?ssl=true&replicaSet=IOT-shard-0&authSource=admin&retryWrites=true&w=majority');
     $db = $connection->JT;
     $collection = $db->IOT;
     var_dump($collection);
